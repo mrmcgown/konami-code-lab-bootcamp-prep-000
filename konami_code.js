51d5ac2d1f511d765e7493278ea8b96dbfa173e7
Michael R. McGown <mrmcgown@gmail.com>
2018-04-30 17:56:03 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,6 +3,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
 document.body.addEventListener('keydown', function(e) {
   const key = parseInt(e.detail || e.which);
-  console.log(key);
+  for (i=0; i<code.length; i++) {
+    if (key === code[i]) {
+      alert("Hurray!");
+    }
+  }
 })
 }
