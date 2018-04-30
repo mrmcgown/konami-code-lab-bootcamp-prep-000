38da8bc1c3fc7ca0af7d5a2aad19104a4202ff06
Michael R. McGown <mrmcgown@gmail.com>
2018-04-30 17:48:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
 document.body.addEventListener('keydown', function(e) {
-  
+  const key = parseInt(e.detail || e.which);
 })
 }
